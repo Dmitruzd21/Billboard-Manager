8f60642961166f6c76748736017743721e971ba6
Korotkov Dmitry <2110821@mail.ru>
2021-08-20 15:27:58 +0300

    Все методы и тесты к ним

--- src/main/java/ru/netology/Manager.java
+++ src/main/java/ru/netology/Manager.java
@@ -6,6 +6,16 @@ import ru.netology.domain.Film;
 
 public class Manager {
     private Film[] films = new Film[0];
+    private int numberOfDisplayedFilms = 10;
+
+    public Manager(int numberOfDisplayedFilms) {
+        this.numberOfDisplayedFilms = numberOfDisplayedFilms;
+    }
+
+    public Manager() {
+        this.numberOfDisplayedFilms = numberOfDisplayedFilms;
+    }
+
 
     // Добавление фильма
     public void save(Film film) {
@@ -19,16 +29,6 @@ public class Manager {
     }
 
     // Возвращает массив всех хранящихся в массиве объектов (в обратном порядке)
-    private int numberOfDisplayedFilms = 10;
-
-    public Manager(int numberOfDisplayedFilms) {
-        this.numberOfDisplayedFilms = numberOfDisplayedFilms;
-    }
-
-    public Manager() {
-        this.numberOfDisplayedFilms = numberOfDisplayedFilms;
-    }
-
 
     public Film[] findLessOrEqual10() {
         int resultLength;
@@ -45,19 +45,6 @@ public class Manager {
         return result;
     }
 
-    // ***Удаление фильма
-    public void removeById(int id) {
-        Film[] tmp2 = new Film[films.length - 1];
-        int index = 0;
-        for (Film film2 : films) {
-            if (film2.getId() != id) {
-                tmp2[index] = film2;
-                index++;
-            }
-        }
-        films = tmp2;
-    }
-
     // ***Возвращает весь массив в прямом порядке
     public Film[] findAll() {
         Film[] result = new Film[films.length];
@@ -67,7 +54,40 @@ public class Manager {
         return result;
     }
 
-    // ***Возвращает массив всех хранящихся в массиве объектов (в обратном порядке)
+    // Возвращает объект по идентификатору (либо null, если такого объекта нет)
+
+    public Film[] findById(int id) {
+        Film[] result = new Film[1];
+        int i = 0;
+        for (Film film : films) {
+            if (film.getId() == id) {
+                result[i] = film;
+                i++;
+            }
+        }
+        return result;
+    }
+
+    // ***Удаление фильма
+    public void removeById(int id) {
+        Film[] tmp2 = new Film[films.length - 1];
+        int index = 0;
+        for (Film film : films) {
+            if (film.getId() != id) {
+                tmp2[index] = film;
+                index++;
+            }
+        }
+        films = tmp2;
+    }
+
+    // ***Полное удаление элементов репозитория
+    public void removeAll() {
+        Film[] empty = new Film[0];
+        films = empty;
+    }
+
+    // ***НЕТ В ДЗ!!!Возвращает массив всех хранящихся в массиве объектов (в обратном порядке)
     public Film[] findAllInReverse() {
         Film[] result = new Film[films.length];
         for (int i = 0; i < films.length; i++) {
@@ -76,33 +96,7 @@ public class Manager {
         }
         return result;
     }
-
-    // ***Полное удаление элементов репозитория
-    public Film[] removeAll() {
-        Film[] empty = new Film[0];
-        films = empty;
-        return films;
-    }
-
 }
-
-
-//Film f1 = new Film(1, "Бладшот", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f2 = new Film(2, "Вперед", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f3 = new Film(3, "Отель Белград", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f4 = new Film(4, "Джентельмены", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f5 = new Film(5, "Человек-Неведимка", "ужасы", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f6 = new Film(6, "Тролли", "мультфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f7 = new Film(7, "Номер один", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f8 = new Film(8, "Кунгфу Панда", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f9 = new Film(9, "Кунгфу Панда 2", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f10 = new Film(10, "Кунгфу Панда 3", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f11 = new Film(11, "Шрек", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f12 = new Film(12, "Шрек 2", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f13 = new Film(13, "Щрек 3", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f14 = new Film(14, "Главный Герой", "фантастика", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-// Film f15 = new Film(15, "Бросок Кобры", "фантастика", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-
 //System.arraycopy (items,0,tmp, 0, items.length)
 //(из какого массива, с какого места, куда копировать,с какого места всавлять, количество элементов которые хотим скопировать)
 
--- src/test/java/ru/netology/ManagerTest.java
+++ src/test/java/ru/netology/ManagerTest.java
@@ -6,11 +6,23 @@ import ru.netology.domain.Film;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagerTest {
+    Film f1 = new Film(1, "Бладшот", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f2 = new Film(2, "Вперед", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f3 = new Film(3, "Отель Белград", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f4 = new Film(4, "Джентельмены", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f5 = new Film(5, "Человек-Неведимка", "ужасы", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f6 = new Film(6, "Тролли", "мультфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f7 = new Film(7, "Номер один", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f8 = new Film(8, "Кунгфу Панда", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f9 = new Film(9, "Кунгфу Панда 2", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f10 = new Film(10, "Кунгфу Панда 3", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
+    Film f11 = new Film(11, "Шрек", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
 
+
+    //Проверка добавления фильма
     @Test
-    public void shouldsaveIfNoOneFilmsExist() {
+    public void shouldSaveIfNoOneFilmsExist() {
         Manager manager = new Manager();
-        Film f1 = new Film(1, "Бладшот", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
         manager.save(f1);
         Film[] expected = new Film[]{f1};
         Film[] actual = manager.findAll();
@@ -18,11 +30,8 @@ class ManagerTest {
     }
 
     @Test
-    public void shouldsaveIfSomeFilmsExist() {
+    public void shouldSaveIfSomeFilmsExist() {
         Manager manager = new Manager();
-        Film f1 = new Film(1, "Бладшот", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f2 = new Film(2, "Вперед", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f3 = new Film(3, "Отель Белград", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
         manager.save(f1);
         manager.save(f2);
         manager.save(f3);
@@ -32,7 +41,7 @@ class ManagerTest {
     }
 
     @Test
-    public void shouldsaveIfTheSameFilmExist() {
+    public void shouldSaveIfTheSameFilmExist() {
         Manager manager = new Manager();
         Film f1 = new Film(1, "Бладшот", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
         Film f2 = new Film(2, "Вперед", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
@@ -47,18 +56,10 @@ class ManagerTest {
         assertArrayEquals(expected, actual);
     }
 
+    //Проверка выдачи последних 10 добавленных фильмов обратном порядке или сколько есть
     @Test
-    public void shouldfindLessOrEqual10If9() {
+    public void shouldFindLessOrEqual10If9() {
         Manager manager = new Manager();
-        Film f1 = new Film(1, "Бладшот", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f2 = new Film(2, "Вперед", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f3 = new Film(3, "Отель Белград", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f4 = new Film(4, "Джентельмены", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f5 = new Film(5, "Человек-Неведимка", "ужасы", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f6 = new Film(6, "Тролли", "мультфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f7 = new Film(7, "Номер один", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f8 = new Film(8, "Кунгфу Панда", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f9 = new Film(9, "Кунгфу Панда 2", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
         manager.save(f1);
         manager.save(f2);
         manager.save(f3);
@@ -74,18 +75,8 @@ class ManagerTest {
     }
 
     @Test
-    public void shouldfindLessOrEqual10IfEqual10() {
+    public void shouldFindLessOrEqual10IfEqual10() {
         Manager manager = new Manager();
-        Film f1 = new Film(1, "Бладшот", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f2 = new Film(2, "Вперед", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f3 = new Film(3, "Отель Белград", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f4 = new Film(4, "Джентельмены", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f5 = new Film(5, "Человек-Неведимка", "ужасы", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f6 = new Film(6, "Тролли", "мультфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f7 = new Film(7, "Номер один", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f8 = new Film(8, "Кунгфу Панда", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f9 = new Film(9, "Кунгфу Панда 2", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f10 = new Film(10, "Кунгфу Панда 3", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
         manager.save(f1);
         manager.save(f2);
         manager.save(f3);
@@ -102,19 +93,8 @@ class ManagerTest {
     }
 
     @Test
-    public void shouldfindLessOrEqual10If11() {
+    public void shouldFindLessOrEqual10If11() {
         Manager manager = new Manager();
-        Film f1 = new Film(1, "Бладшот", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f2 = new Film(2, "Вперед", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f3 = new Film(3, "Отель Белград", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f4 = new Film(4, "Джентельмены", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f5 = new Film(5, "Человек-Неведимка", "ужасы", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f6 = new Film(6, "Тролли", "мультфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f7 = new Film(7, "Номер один", "комедия", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f8 = new Film(8, "Кунгфу Панда", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f9 = new Film(9, "Кунгфу Панда 2", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f10 = new Film(10, "Кунгфу Панда 3", "мульфильм", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
-        Film f11 = new Film(11, "Шрек", "боевик", "https://github.com/netology-code/javaqa-homeworks/blob/master/dependency/pic/afisha.png", false);
         manager.save(f1);
         manager.save(f2);
         manager.save(f3);
@@ -132,7 +112,7 @@ class ManagerTest {
     }
 
     @Test
-    public void shouldfindLessOrEqual10IfNoOneExist() {
+    public void shouldFindLessOrEqual10IfNoOneExist() {
         Manager manager = new Manager();
         Film[] expected = new Film[0];
         Film[] actual = manager.findLessOrEqual10();
@@ -140,5 +120,121 @@ class ManagerTest {
 
     }
 
+    //  Проверка возвращения объекта по идентификатору (либо null, если такого объекта нет)
+    @Test
+    public void shoildfindByIdIfIdExist() {
+        Manager manager = new Manager();
+        manager.save(f1);
+        manager.save(f2);
+        manager.save(f3);
+        manager.save(f4);
+        manager.save(f5);
+        Film[] expected = new Film[]{f3};
+        Film[] actual = manager.findById(3);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shoildfindByIdIfIdNoExist() {
+        Manager manager = new Manager();
+        manager.save(f1);
+        manager.save(f2);
+        manager.save(f4);
+        manager.save(f5);
+        Film[] expected = new Film[]{null};
+        Film[] actual = manager.findById(3);
+        assertArrayEquals(expected, actual);
+    }
+
+    //Проверка удаления объекта по идентификатору
+    @Test
+    public void shouldRemoveByIdIfManyFilmsExist() {
+        Manager manager = new Manager();
+        manager.save(f1);
+        manager.save(f2);
+        manager.save(f3);
+        manager.save(f4);
+        manager.save(f5);
+        manager.save(f6);
+        manager.save(f7);
+        manager.removeById(4);
+        Film[] expected = new Film[]{f1, f2, f3, f5, f6, f7};
+        Film[] actual = manager.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveByIdIfOneFilmExist() {
+        Manager manager = new Manager();
+        manager.save(f1);
+        manager.removeById(1);
+        Film[] expected = new Film[0];
+        Film[] actual = manager.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    //Проверка полного очищения репозитория
+    @Test
+    public void shouldRemoveAll() {
+        Manager manager = new Manager();
+        manager.save(f1);
+        manager.save(f2);
+        manager.save(f3);
+        manager.save(f4);
+        manager.save(f5);
+        manager.save(f6);
+        manager.save(f7);
+        manager.save(f8);
+        manager.save(f9);
+        manager.removeAll();
+        Film[] expected = new Film[0];
+        Film[] actual =manager.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveAllIfNoOneExist() {
+        Manager manager = new Manager();
+        manager.removeAll();
+        Film[] expected = new Film[0];
+        Film[] actual = manager.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+//НЕТ В ДЗ!!! Проверка возвращения всех элементов массива в прямом порядке
+    @Test
+    public void shouldFindAllInReverseIfManyFilmsExist() {
+        Manager manager = new Manager();
+        manager.save(f1);
+        manager.save(f2);
+        manager.save(f3);
+        manager.save(f4);
+        manager.save(f5);
+        manager.save(f6);
+        manager.save(f7);
+        manager.save(f8);
+        manager.save(f9);
+        Film[] expected = new Film[]{f9, f8, f7, f6, f5, f4, f3, f2, f1};
+        Film[] actual = manager.findAllInReverse();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindAllInReverseIfOneExist() {
+        Manager manager = new Manager();
+        manager.save(f1);
+        Film[] expected = new Film[]{f1};
+        Film[] actual = manager.findAllInReverse();
+        assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldFindAllInReverseIfNoOneExist() {
+        Manager manager = new Manager();
+        Film[] expected = new Film[0];
+        Film[] actual = manager.findAllInReverse();
+        assertArrayEquals(expected, actual);
+    }
 
 }
